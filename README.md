41509631ec99a8152ecb38f4eebcc3279a3ca0e5
zeinabkeita2025-maker <zeinabkeita2025@icloud.com>
2026-06-17 03:36:04 +0100

    Deploiement : Github Pages activé, README finalisé et presentation Powerpoint ajoutée

--- README.md
+++ README.md
@@ -3,3 +3,142 @@ Projet fil rouge — Plateforme de mise en relation entre freelances africains e
 clients.
 Auteur : Djenebou keita
 Promotion : L1 Web — ISI
+
+# AfriTalent Tech 🌍💻
+
+# 📖 Présentation
+**AfriTalent Tech** est une plateforme web développée pour faciliter le recrutement de talents numériques en Afrique. Elle permet aux entreprises de trouver rapidement des freelances qualifiés et offre aux professionnels africains un espace pour valoriser leurs compétences et développer leur carrière.
+Le projet a été conçu comme une application front-end moderne mettant en pratique les concepts essentiels du développement web : responsive design, composants Bootstrap, interactions JavaScript et expérience utilisateur.
+
+## ✨ Fonctionnalités principales
+
+### 🏠 Page d'accueil
+
+* Présentation de la plateforme ;
+* Statistiques et témoignages ;
+* Catégories de services ;
+* Mode sombre ;
+* Bouton retour en haut.
+
+### 👨🏾‍💻 Gestion des freelances
+
+* Catalogue des profils ;
+* Filtrage par catégories ;
+* Fiches détaillées des freelances.
+
+### 📞 Communication
+
+* Formulaire de contact ;
+* Coordonnées de l'entreprise ;
+* Carte Google Maps.
+
+### 📰 Contenu et informations
+
+* Blog avec conseils pratiques ;
+* Newsletter ;
+* Page « À propos » présentant l'équipe et les valeurs.
+
+### 💰 Tarifs et assistance
+
+* Différentes offres d'abonnement ;
+* FAQ interactive.
+
+---
+
+## 🛠️ Technologies utilisées
+
+* HTML5
+* CSS3
+* JavaScript
+* Bootstrap 5
+* Bootstrap Icons
+* Google Fonts (Poppins)
+
+---
+
+## 📁 Structure du projet
+
+```
+Keita-Djénébou -AfriTaLent/					
+│  .gitignore
+│   about.html
+│   blog.html
+│   contact.html
+│   freelances.html
+│   index.html
+│   README.md
+│   tarifs.html
+├───css							 
+│    style.css  
+|    index.css 
+|    freelances.css
+|    contact.css  
+|    blog.css  
+|    about.css  
+|    tarifs.css  
+|    style.css
+├───docs						
+│       Keita_Djénébou.pptx       						
+├───images
+│       
+└───js
+        main.js
+
+```
+## Les captures d'ecran des pages
+ # 📸 Aperçu du projet
+
+## 🏠 Page d'accueil
+![Page d'accueil](images/capture-accueil.png)
+---
+## 👨🏾‍💻 Catalogue des freelances
+![Catalogue des freelances](images/capture-freelances.png)
+---
+## 📞 Page Contact
+![Page Contact](images/capture-contact.png)
+---
+## 📰 Blog
+![Page Blog](images/capture-blog.png)
+---
+## 🏢 À propos
+
+![Page À propos](images/capture-about.png)
+---
+## 💰 Tarifs et FAQ
+![Page Tarifs](images/capture-Tarifs.png)
+
+## 📱 Responsive Design
+L'interface est optimisée pour :
+* 💻 Ordinateurs;
+* 📱 Smartphones;
+* 📟 Tablettes;
+
+Bootstrap garantit une expérience utilisateur fluide sur tous les écrans.
+---
+## lien visionnaire
+https://zeinabkeita2025-ship-it.github.io/Keita-Djenebou-AfriTalent
+## Le lien github
+https://github.com/zeinabkeita2025-ship-it/Keita-Djenebou-AfriTalent
+
+## 🔮 Évolutions futures
+
+Les fonctionnalités suivantes pourraient être ajoutées :
+
+* Authentification (connexion / inscription) ;
+* Gestion des rôles ;
+* Tableau de bord recruteurs ;
+* Tableau de bord freelances ;
+* Publication dynamique des projets ;
+* Base de données ;
+* API backend ;
+* Messagerie interne ;
+* Paiement sécurisé ;
+* Recherche avancée ;
+* Administration du blog ;
+* Notifications en temps réel.
+
+---
+
+###  Vision du projet
+
+**AfriTalent Tech** ambitionne de devenir un pont entre les entreprises africaines et les talents du numérique afin de contribuer au développement de l'écosystème technologique du continent.
--- js/main.js
+++ js/main.js
@@ -15,7 +15,7 @@ document.addEventListener('DOMContentLoaded', function () { // fonction qui atte
                 toggleBtn.textContent = "🌙";
             } else {
                 body.classList.replace("light", "dark");
-                localStorage.setItem("theme", "dark"); 
+                localStorage.setItem("theme", "dark");
                 toggleBtn.textContent = "⭐";
             }
         });
@@ -55,7 +55,7 @@ document.addEventListener('DOMContentLoaded', function () { // fonction qui atte
                             el.textContent = Math.floor(count);
                             requestAnimationFrame(update);//Demande au navigateur d'exécuter update() à la prochaine image (environ 60 fois par seconde)AF
                         } else {
-                            el.textContent = target; 
+                            el.textContent = target;
                         }
                     };
                     update(); //Lance l'animation du compteur.
@@ -65,101 +65,106 @@ document.addEventListener('DOMContentLoaded', function () { // fonction qui atte
         }, { threshold: 1 }); //quand tout l'élément visible à l'écran(100%)
         counters.forEach(c => observer.observe(c)); //On demande à l'observateur de surveiller chaque élément .counter.
     }
-
-    // ===== FADE IN =====
-    const sections = document.querySelectorAll(".fade");
-    if (sections.length > 0) {
-        const fadeObserver = new IntersectionObserver((entries) => {
-            entries.forEach(entry => {
-                if (entry.isIntersecting) entry.target.classList.add("show");
-            });
-        }, { threshold: 0.2 }); //quand 20% de l'element apparait à l'écran
-        sections.forEach(s => fadeObserver.observe(s)); 
-    }
-
-     // ===== FILTRE FREELANCES ===== //
-     //selection des elements (categories et cartes)
-    const filtreButtons = document.querySelectorAll(".filtre");
-    const cards = document.querySelectorAll(".freelancer-card");
-    if (filtreButtons.length > 0) { //verifie s'il y'a au moins un bouton de filtrage
-        filtreButtons.forEach(button => { //on parcout les boutons de filtrage one by one 
-            button.addEventListener('click', function () { //quand on clique sur un bouton
-                const filtre = button.dataset.category; // on recupre la valeur de ce bouton(genre la categorie attrubuer data-target)
-                cards.forEach(card => { // on parcourt alors les cartes one by one
-                    const col = card.closest(".col-md-4"); //on remonte alors jusqu'au parent col-md-4 en cherchant la colonne qui a la categorie selectionné
-                    if (filtre === 'tous') {
-                        col.classList.remove("hidden"); // on affiche toutes les cartes
-                    } else {
-                        col.classList.toggle("hidden", card.dataset.category !== filtre);// on affiche les cartes ayant la categorie selectionné
-                    }
-                });
-            });
-        });
-    }
 });
-    // ===== FORMULAIRE =====
-    const Form = document.getElementById("contactForm");
-    if (Form) {
-        const nom = document.getElementById("name");
-        const prenom = document.getElementById("prenom");
-        const email = document.getElementById("email");
-        const message = document.getElementById("message");
-        const telephone = document.getElementById("telephone");
-        // les messages d'erreurs
-        const errorNom = document.getElementById("nameError");
-        const errorPrenom = document.getElementById("prenomError");
-        const errorEmail = document.getElementById("emailError");
-        const errorTelephone = document.getElementById("telephoneError");
-        const errorMessage = document.getElementById("messageError");
-        const succes = document.getElementById("succes");
-        const input = document.querySelector('input');
-        //fonction e qui controle 
-        Form.addEventListener("submit", function (e){
-            e.preventDefault(); //empeche l'envoie
-            errorNom.textContent = "";
-            errorEmail.textContent = "";
-            errorMessage.textContent = "";
-            errorPrenom.textContent = "";
-            errorTelephone.textContent = ""; 
-            succes.textContent = "";
-
-            let isValide = true;
-            if (nom.value.trim() == "") { //recupère la valeur saisie sans espace entre eux
-                errorNom.textContent = "Le nom est obligatoire"; 
-                isValide = false; 
-                errorNom.style.color = 'red'; 
-            }
-             if (prenom.value.trim() == "") { 
-                errorPrenom.textContent = "Le prenom est obligatoire"; 
-                isValide = false; 
-                errorPrenom.style.color = 'red'; 
-            }
-            if (email.value.trim() == "") { 
-                errorEmail.textContent = "L'email est obligatoire"; 
-                isValide = false; 
-                errorEmail.style.color = 'red';
-                errorEmail.style.borderColor = 'red';
-            }
-            else if (!/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email.value)) {
-                 errorEmail.textContent = "Email invalide";
-                  isValide = false;
-                  errorEmail.style.color = 'red';
-                }
-            if (message.value.trim().length < 20) { 
-                errorMessage.textContent = " le message doit contenir au minimum 20 caractères";
-                 isValide = false;
-                 errorMessage.style.color = 'red';
-                 }
-            if (telephone.value.trim() == "") { 
-                errorTelephone.textContent = "Le champ est obligatoire"; 
-                isValide = false;
-            errorTelephone.style.color = 'red';
-         }
-            if (isValide) { 
-                succes.textContent = "Envoyé avec succès !";
-                succes.style.color = 'green'; 
-                 Form.reset(); // permet de recharger la page genre vider le formulaire
-                }
+// ===== FADE IN =====
+const sections = document.querySelectorAll(".fade");
+if (sections.length > 0) {
+    const fadeObserver = new IntersectionObserver((entries) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) entry.target.classList.add("show");
         });
-    }
-    
+    }, { threshold: 0.2 }); //quand 20% de l'element apparait à l'écran
+    sections.forEach(s => fadeObserver.observe(s));
+}
+
+// ===== FILTRE FREELANCES ===== //
+//selection des elements (categories et cartes)
+const filtreButtons = document.querySelectorAll(".filtre");
+const cards = document.querySelectorAll(".freelancer-card");
+if (filtreButtons.length > 0) { //verifie s'il y'a au moins un bouton de filtrage
+    filtreButtons.forEach(button => { //on parcout les boutons de filtrage one by one 
+        button.addEventListener('click', function () { //quand on clique sur un bouton
+            const filtre = button.dataset.category; // on recupre la valeur de ce bouton(genre la categorie attrubuer data-target)
+            cards.forEach(card => { // on parcourt alors les cartes one by one
+                const col = card.closest(".col-md-4"); //on remonte alors jusqu'au parent col-md-4 en cherchant la colonne qui a la categorie selectionné
+                if (filtre === 'tous') {
+                    col.classList.remove("hidden"); // on affiche toutes les cartes
+                } else {
+                    col.classList.toggle("hidden", card.dataset.category !== filtre);// on affiche les cartes ayant la categorie selectionné
+                }
+            });
+        });
+    });
+}
+
+// ===== FORMULAIRE =====
+const Form = document.getElementById("contactForm");
+if (Form) {
+    const nom = document.getElementById("name");
+    const prenom = document.getElementById("prenom");
+    const email = document.getElementById("email");
+    const message = document.getElementById("message");
+    const telephone = document.getElementById("telephone");
+    // les messages d'erreurs
+    const errorNom = document.getElementById("nameError");
+    const errorPrenom = document.getElementById("prenomError");
+    const errorEmail = document.getElementById("emailError");
+    const errorTelephone = document.getElementById("telephoneError");
+    const errorMessage = document.getElementById("messageError");
+    const succes = document.getElementById("succes");
+    const input = document.querySelector('input');
+    //fonction e qui controle 
+    Form.addEventListener("submit", function (e) {
+        e.preventDefault(); //empeche l'envoie
+        errorNom.textContent = "";
+        errorEmail.textContent = "";
+        errorMessage.textContent = "";
+        errorPrenom.textContent = "";
+        errorTelephone.textContent = "";
+        succes.textContent = "";
+        //validation du nom
+        let isValide = true;
+        if (nom.value.trim() == "") { //recupère la valeur saisie sans espace entre eux
+            errorNom.textContent = "Le nom est obligatoire";
+            isValide = false;
+            errorNom.style.color = 'red';
+        }
+        //Validation du prenom
+        if (prenom.value.trim() == "") {
+            errorPrenom.textContent = "Le prenom est obligatoire";
+            isValide = false;
+            errorPrenom.style.color = 'red';
+        }
+        //Validation de l'email
+        if (email.value.trim() == "") {
+            errorEmail.textContent = "L'email est obligatoire";
+            isValide = false;
+            errorEmail.style.color = 'red';
+            errorEmail.style.borderColor = 'red';
+        }
+
+        else if (!/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email.value)) {
+            errorEmail.textContent = "Email invalide";
+            isValide = false;
+            errorEmail.style.color = 'red';
+        }
+        //Validation du message
+        if (message.value.trim().length < 20) {
+            errorMessage.textContent = " le message doit contenir au minimum 20 caractères";
+            isValide = false;
+            errorMessage.style.color = 'red';
+        }
+        //Validation du telephone
+        if (telephone.value.trim() == "") {
+            errorTelephone.textContent = "Le champ est obligatoire";
+            isValide = false;
+            errorTelephone.style.color = 'red';
+        }
+        //Message du succes
+        if (isValide) {
+            succes.textContent = "Envoyé avec succès !";
+            succes.style.color = 'green';
+            Form.reset(); // permet de recharger la page genre vider le formulaire
+        }
+    });
+}
